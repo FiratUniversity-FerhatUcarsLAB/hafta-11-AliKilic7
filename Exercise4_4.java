3381d6ccd2be1b3275263a206c3a4a1b076c120c
S Ali KILIÇ <250541036@firat.edu.tr>
2025-12-03 22:06:11 +0300

    Update Exercise4_4.java

--- Exercise4_4.java
+++ Exercise4_4.java
@@ -19,3 +19,24 @@ public class Exercise4_4 {
         // Cevaplarinizi yorum olarak ekleyin.
     }
 }
+
+/*
+--------------------------------------------------------------------
+SORU 1: Return eden metodu çağırıp sonucu kullanmazsak ne olur?
+--------------------------------------------------------------------
+Cevap:
+- Java hiçbir hata veya uyarı vermez.
+- Metot normal şekilde çalışır.
+- Dönen değer kullanılmadığı için atılır.
+
+--------------------------------------------------------------------
+SORU 2: Void metodu bir ifadenin içinde kullanırsak ne olur?
+Örnek: System.out.println(sayHello() + 7);
+--------------------------------------------------------------------
+Cevap:
+- Derleme hatası verir:
+    "void type not allowed here"
+- Çünkü void türü bir değer döndürmez.
+- + operatörü bir değer bekler, void ile kullanılamaz.
+--------------------------------------------------------------------
+*/
